4dedba30a772616d2a7ee186dd97fcff3cb36636
jungai <juworaphol125@gmail.com>
2021-10-13 23:29:31 +0700

    move to posts folder

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,7 @@ const IndexPage = () => {
 	return (
 		<div className="grid place-content-center h-screen">
 			<h1 className="text-2xl text-[tomato]">Karuta Blog</h1>
-			<Link to="/command">commands</Link>
+			<Link to="/posts/command">commands</Link>
 		</div>
 	);
 };
