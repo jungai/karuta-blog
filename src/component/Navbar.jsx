915ca331af4319dcc750fab00b7c144e18344472
opal <opal@prodigy9.co>
2021-10-14 23:59:51 +0700

    update navbar and home

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,7 +1,22 @@
 import { Link } from 'gatsby'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Navbar = () => {
+	const [wording, setWording] = useState('What is tomato?')
+
+	if (typeof window !== 'undefined') {
+		window.onscroll = () => scrollFn()
+	}
+
+	const scrollFn = () => {
+		const scroller = document.documentElement.scrollTop
+		if (scroller < 50) {
+			setWording('What is tomato?')
+		} else {
+			setWording('tomatoooooooooooooo:o')
+		}
+	}
+
 	return (
 		<nav className="sticky top-0 w-full flex items-center justify-between border-b border-black px-20 py-3 bg-white">
 			<div className="flex items-center flex-shrink-0 mr-10 border-l-4 border-[tomato]">
@@ -10,15 +25,12 @@ const Navbar = () => {
 				</span>
 			</div>
 			<div className="flex space-x-6 text-sm">
-				<Link to="/posts/command">
-					<div>
-						<a className="hover:text-[tomato]">Karuta</a>
-					</div>
+				<p>{wording}</p>
+				<Link to="/posts/command" className="hover:text-[tomato]">
+					Karuta
 				</Link>
-				<Link to="/posts/command">
-					<div>
-						<a className="hover:text-green-500">Blog</a>
-					</div>
+				<Link to="/posts/command" className="hover:text-green-500">
+					Blog
 				</Link>
 			</div>
 		</nav>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import Navbar from '../component/Navbar'
 
 const IndexPage = () => {
 	return (
 		<>
 			<Navbar />
-			<div className="min-h-screen">
+			<div>
 				<div className="h-96 bg-[tomato] bg-gradient-to-t from-green-500 px-20 py-2">
-					<h1 className="text-2xl ">Karuta Blog</h1>
-					<Link to="/posts/command">commands</Link>
+					<h1 className="text-9xl text-green-500">tomato</h1>
 				</div>
 				<div className="h-96 bg-green-500 bg-gradient-to-t from-white px-20 py-2"></div>
 			</div>
