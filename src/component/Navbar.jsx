a9f7a27deabcf122ce374d4f81e1164569d216b5
jungai <juworaphol125@gmail.com>
2021-10-15 21:05:58 +0700

    tomato in tailwind

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -19,14 +19,14 @@ const Navbar = () => {
 
 	return (
 		<nav className="sticky top-0 w-full flex items-center justify-between border-b border-black px-20 py-3 bg-white">
-			<div className="flex items-center flex-shrink-0 mr-10 border-l-4 border-[tomato]">
+			<div className="flex items-center flex-shrink-0 mr-10 border-l-4 border-tomato">
 				<span className="w-full text-center font-semibold text-2xl tracking-tight px-1">
 					eiei<span className="text-green-500">.</span>ie
 				</span>
 			</div>
 			<div className="flex space-x-6 text-sm">
 				<p>{wording}</p>
-				<Link to="/posts/command" className="hover:text-[tomato]">
+				<Link to="/posts/command" className="hover:text-tomato">
 					Karuta
 				</Link>
 				<Link to="/posts/command" className="hover:text-green-500">
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,7 +6,7 @@ const IndexPage = () => {
 		<>
 			<Navbar />
 			<div>
-				<div className="h-96 bg-[tomato] bg-gradient-to-t from-green-500 px-20 py-2">
+				<div className="h-96 bg-tomato bg-gradient-to-t from-green-500 px-20 py-2">
 					<h1 className="text-9xl text-green-500">tomato</h1>
 				</div>
 				<div className="h-96 bg-green-500 bg-gradient-to-t from-white px-20 py-2"></div>
