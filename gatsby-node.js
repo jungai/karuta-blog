ee4c907a687c97ed32bc9c7cb64600e970d3d656
jungai <juworaphol125@gmail.com>
2021-10-17 14:55:21 +0700

    use gatsby node api for build dynamic page

---
+++ gatsby-node.js
@@ -0,0 +1,34 @@
+const path = require(`path`)
+
+// use node to create dynamic page
+exports.createPages = async ({ graphql, actions }) => {
+	const { createPage } = actions
+
+	const rawResult = await graphql(`
+		{
+			allMarkdownRemark(filter: { fileAbsolutePath: { regex: "/contents/" } }) {
+				edges {
+					node {
+						frontmatter {
+							title
+							author
+							date
+							slug
+						}
+						html
+					}
+				}
+			}
+		}
+	`)
+
+	const content = rawResult.data.allMarkdownRemark.edges
+
+	content.forEach(({ node }) => {
+		createPage({
+			path: node.frontmatter.slug,
+			component: path.resolve('./src/templates/blog.jsx'),
+			context: {},
+		})
+	})
+}
